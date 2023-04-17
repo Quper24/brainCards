4e3253172f044e23aeb05852077b9447079c1c54
Maksim Leskin <maksim.leskin@skillbox.ru>
2023-04-18 01:16:38 +0300

    day2

--- js/components/createCategory.js
+++ js/components/createCategory.js
@@ -1,4 +1,5 @@
 import { createElement } from '../helper/createElement.js'
+import { declOfNum } from '../helper/declOfNum.js';
 
 export const createCategory = (app) => {
   const category = createElement('section', {
@@ -20,23 +21,46 @@ export const createCategory = (app) => {
   const createCategoryCard = (data) => {
     const item = createElement('li', {
       className: 'category__item',
-      textContent: data.title, // убрать
     });
     
     item.dataset.id = data.id;
 
-    /**
-     * Написать функционал создания html карточки
-     */
+    const btnCard = createElement('button', {
+      className: 'category__card',
+    });
+
+    const titleText = createElement('span', {
+      className: 'category__title',
+      textContent: data.title,
+    });
+
+    const countPairs = createElement('span', {
+      className: 'category__pairs',
+      textContent: declOfNum(data.length, ['пара', 'пары', 'пар'])
+    });
+
+    btnCard.append(titleText, countPairs);
+
+    const btnEdit = createElement('button', {
+      className: 'category__btn category__edit',
+      ariaLabel: 'редактировать',
+    });
+
+    const btnDel = createElement('button', {
+      className: 'category__btn category__del',
+      ariaLabel: 'удалить',
+    });
+    
+    item.append(btnCard, btnEdit, btnDel);
 
     return item;
   }
 
   const mount = (data) => {
     categoryList.textContent = '';
-    app.append(category);
     const cards = data.map(createCategoryCard);
     categoryList.append(...cards);
+    app.append(category);
   };
 
   const unmount = () => {
